8b17dadbff656de0062a6b137ed69da9dabc8459
anthonymateo <anthonymateodelosreyes.18@gmail.com>
2025-06-28 15:58:01 -0600

    intento006

---
+++ src/Header.jsx
@@ -0,0 +1,17 @@
+const Header = () => {
+  return (
+    <header>
+      <h1 className='header'></h1>
+      <nav className='title'>
+        Hola World
+        <ul>
+          <li>
+            <a href='#'>Pero cambia?</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
